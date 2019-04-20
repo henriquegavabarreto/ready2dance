cf17f8b3827a68a1c4d726a6bc3af7790a82ce14
henrique <henriquegavabarreto@gmail.com>
2019-04-19 22:14:36 -0400

    change icons, add timing modal file

--- src/components/Editorheader.vue
+++ src/components/Editorheader.vue
@@ -1,27 +1,61 @@
 <template>
   <div>
-    <v-toolbar dark color="primary">
-      <v-btn color="primary">Timing
-        <v-icon>timer</v-icon>
-      </v-btn>
-      <v-btn color="primary">Song
-        <v-icon>audiotrack</v-icon>
-      </v-btn>
-      <v-btn color="primary">Load Video
-        <v-icon>movie</v-icon>
-      </v-btn>
-      <v-btn color="primary">
-        save chart
-        <v-icon>save</v-icon>
-      </v-btn>
-      <v-btn color="primary">
-        info
-        <v-icon>info</v-icon>
-      </v-btn>
-      <v-btn color="primary">
-        Back
-        <v-icon>exit_to_app</v-icon>
-      </v-btn>
+    <v-toolbar dark>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">
+            <v-icon>timer</v-icon>
+          </v-btn>
+        </template>
+        <span class="body-2">Timing</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">
+            <v-icon>audiotrack</v-icon>
+          </v-btn>
+        </template>
+        <span class="body-2">Song Info</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">
+            <v-icon>video_library</v-icon>
+          </v-btn>
+        </template>
+        <span class="body-2">Load Video</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">
+            <v-icon>done_outline</v-icon>
+          </v-btn>
+        </template>
+        <span class="body-2">Save Chart</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">
+            <v-icon>info</v-icon>
+          </v-btn>
+        </template>
+        <span class="body-2">About this editor</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">
+            <v-icon>exit_to_app</v-icon>
+          </v-btn>
+        </template>
+        <span class="body-2">Back to Song Selection</span>
+      </v-tooltip>
+
     </v-toolbar>
   </div>
 </template>
