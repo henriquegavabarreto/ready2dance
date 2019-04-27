fda95689ef83c2e528f1c536f3be56a1e94679c7
henrique <henriquegavabarreto@gmail.com>
2019-04-27 13:43:21 -0400

    add posenet and media capture to game scene

--- src/scenes/Game.vue
+++ src/scenes/Game.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-
+    <video id="videoStream" style="width: 600px; height: 600px; display: none;">
+    </video>
+    <v-btn @click="estimatePose">Estimate</v-btn>
   </div>
 </template>
 
@@ -8,8 +10,37 @@
 import getUserMedia from 'getusermedia'
 
 export default {
+  data () {
+    return {
+      stream: null,
+      hideStream: true,
+      poseNetOptions: {
+        scale: 0.5,
+        output: 16,
+        flipHorizontal: false
+      }
+    }
+  },
   mounted () {
-
+    getUserMedia({ video: true, audio: false }, (err, stream) => {
+      if (err) {
+        console.log(err)
+      } else {
+        this.stream = document.getElementById('videoStream')
+        this.stream.width = 600
+        this.stream.height = 600
+        this.stream.srcObject = stream
+        this.stream.play()
+        console.log('got a stream', stream)
+      }
+    })
+  },
+  methods: {
+    estimatePose: function () {
+      this.$store.state.net.estimateSinglePose(this.stream, this.poseNetOptions.scale, this.poseNetOptions.flipHorizontal, this.poseNetOptions.output).then((data) => {
+        console.log(data)
+      })
+    }
   }
 }
 </script>
