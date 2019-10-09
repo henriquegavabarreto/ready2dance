ddaf3078881875dd5a553d9785e099d40ca1988e
henrique <henriquegavabarreto@gmail.com>
2019-10-09 19:31:04 -0400

    add comments to store mutations

--- src/store.js
+++ src/store.js
@@ -35,52 +35,67 @@ export default new Vuex.Store({
     previousScene: ''
   },
   mutations: {
+    // show welcome message or not
     toggleWelcome: (state, data) => {
       state.welcome = data
     },
+    // change user according to user that logged in
     changeUser: (state, data) => {
       state.user = data
     },
+    // changes selected song object
     selectSong: (state, data) => {
       state.selectedSong = data
     },
+    // change chartId so a specific chart can be loaded
     selectChart: (state, data) => {
       state.selectedChartId = data
     },
+    // select difficulty to retrieve correct score board and chart
     selectDifficulty: (state, data) => {
       state.selectedDifficulty = data
     },
+    // add song to songs - set initially or if a song is pushed to the database
     addSong: (state, data) => {
       if (state.songs === null) {
         state.songs = {}
       }
       Vue.set(state.songs, data.key, data.val)
     },
+    // make changes to a song and updates it
     changeSong: (state, data) => {
       Vue.set(state.songs, data.key, data.val)
     },
+    // remove song locally
     removeSong: (state, data) => {
       Vue.delete(state.songs, data.key)
     },
+    // change scene to be dynamically loaded by vue
     goToScene: (state, data) => {
       state.previousScene = state.currentScene
       state.currentScene = data
     },
+    // select a specific chart
     changeSelectedChart: (state, data) => {
       state.selectedChart = data
     },
+    // loads posenet
     loadNet: (state, data) => {
       state.net = data
     },
+    // change result to be shown
     changeResults: (state, data) => {
       state.results = data
     },
+    // toggle something went wrong to show error message
     somethingWentWrong: state => {
       state.somethingWentWrong = true
     },
+    // change wrong message content
     changeWrongMessage: (state, data) => {
       state.wrongMessage = data
     },
+    // assign values changed in the settings to the store
     changeOptions: (state, data) => {
       state.gameOptions.showAnimation = data.showAnimation
       state.gameOptions.showWebcam = data.showWebcam
@@ -90,15 +105,19 @@ export default new Vuex.Store({
       state.gameOptions.outputStride = parseInt(data.outputStride)
       state.gameOptions.imageScale = parseFloat(data.imageScale)
     },
+    // change only posenet multiplier
     changeMultiplier: (state, data) => {
       state.gameOptions.multiplier = data
     },
+    // change latency
     changeLatency: (state, data) => {
       state.gameOptions.latency = data
     },
+    // change song scores locally
     changeSongScores: (state, data) => {
       state.songScores = data
     },
+    // change sign in state of an user
     changeState: (state, data) => {
       state.signInState = state
     }
